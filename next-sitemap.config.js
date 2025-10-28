b3419be66392de71d8133e7ca61567091a0f662d
Your Name <tatendagori@gmail.com>
2025-10-28 16:15:17 +0200

    Fix sitemap XML

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://mentorship-app-one.vercel.app/",
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
-};
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/google:verification(.+).html',
-        destination: '/api/verify-google',
-      },
-    ]
-  },
+  siteUrl: 'https://mentorship-app-one.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  outDir: 'public',
 }
-
-module.exports = nextConfig
