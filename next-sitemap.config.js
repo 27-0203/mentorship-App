945a9f1b5372a856c6e107ef943a8da8774b91ef
Your Name <tatendagori@gmail.com>
2025-10-28 19:57:07 +0200

    Add simple static sitemap and robots.txt

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://mentorship-app-one.vercel.app',
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 5000,
-};
